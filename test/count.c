996a2c296d909c75d45ccd7180c31b1b4b564cd2
Alabi Khalid <alabikhalid.12@gmail.com>
2022-04-18 15:54:38 -0700

    added counter--> to count printed string, set up test cases and also updated %s

---
+++ test/count.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _count - count the string 
+ * @ptr: counter
+ * @str: string to count
+ */
+void _count(int *ptr, char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		*ptr += 1;
+	}
+}
---
+++ test/printf.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i = 0, len = 0, k, j, counter = 0;	
+	char *dest = NULL;
+	char *argStr;
+	
+	while (format[len] != '\0')
+		len++;
+
+	dest = malloc(sizeof(char) * len);
+	if (dest == NULL)
+		exit(1);
+
+	va_start(ap, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] == 'c')
+		{
+			argStr = malloc(sizeof(char) * 2);
+			argStr[0] = (char)va_arg(ap, int);
+			argStr[1] = '\0';
+			_count(&counter, argStr);
+			_sprintf(argStr);
+			free(argStr);
+			i += 2;
+		}
+		else if (format[i] == '%' && format[i + 1] == 's')
+		{
+		}
+		else
+		{
+			for (j = i, k = 0; format[j] != '\0' ; k++, j++, i++)
+			{
+				if (format[j] == '%')
+				{
+					i = j;
+					break;
+				}
+				else
+				{
+					dest[k] = format[j];
+				}
+			}
+			dest[k] = '\0';
+			_count(&counter, dest);
+			_sprintf(dest);
+		}
+	}
+
+	return(counter);
+}
