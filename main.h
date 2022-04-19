12f6ea568fff85b67afd740ee819261ed1b9e762
Abuka-Victor <abukavictoro@gmail.com>
2022-04-19 01:18:06 +0100

    Add new include for limit.h

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <limit.h>
 
 int _sprintf(const char *str);
 int _printf(const char *format, ...);
---
+++ test/README.md
@@ -0,0 +1,1 @@
+Readme for Test dir
